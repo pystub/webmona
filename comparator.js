63c09964d55e646887323ba27e2d1599e3864cba
infoburp <gwolfendale@gmail.com>
2014-05-23 19:30:49 +0100

    comments in comparator

--- comparator.js
+++ comparator.js
@@ -1,23 +1,29 @@
-var refData
+//ref data variable
+var refData;
 onmessage = function (event) {
 	//compare images
-	var newData = new Uint8ClampedArray (event.data)
-
+	//new data variable
+	var newData = new Uint8ClampedArray (event.data);
+	//if no ref data 
 	if (!refData || refData.length != newData.length) {
-		refData = newData
-		return
+		//set ref data to new data
+		refData = newData;
+		//return nothing
+		return;
 	}
-
 	var delta = 0
 	for (var i = refData.length; i > 0;) {
 		// tricky reverse cycles again
 		delta +=
-			Math.abs (refData[--i] - newData[i]) + // A
-			Math.abs (refData[--i] - newData[i]) + // B
-			Math.abs (refData[--i] - newData[i]) + // G
-			Math.abs (refData[--i] - newData[i])   // R
+			//alpha
+			Math.abs (refData[--i] - newData[i]) + 
+			//blue
+			Math.abs (refData[--i] - newData[i]) + 
+			//green
+			Math.abs (refData[--i] - newData[i]) + 	
+			//red
+			Math.abs (refData[--i] - newData[i]);   
 	}
-
-
+	//post message back to main script
 	postMessage (delta)
 }

ac71c805895298be0cb6b244ebd952b74d1bcca0
infoburp <gwolfendale@gmail.com>
2014-05-24 12:35:06 +0100

    readability improvements

--- comparator.js
+++ comparator.js
@@ -5,19 +5,21 @@ onmessage = function (event) {
 	//new data variable
 	var newData = new Uint8ClampedArray (event.data);
 	//if no ref data 
-	if (!refData || refData.length != newData.length) {
-		//set ref data to new data
-		refData = newData;
-		return;
-	}
+	if (!refData || refData.length != newData.length) 
+		{
+			//set ref data to new data
+			refData = newData;
+			return;
+		}
 	var delta = 0
-	for (var i = refData.length; i > 0;) {
-		// tricky reverse cycles again
-		delta += Math.abs (refData[--i] - newData[i]) 
-		+ Math.abs (refData[--i] - newData[i]) 
-		+ Math.abs (refData[--i] - newData[i]) 
-		+ Math.abs (refData[--i] - newData[i]);
-	}
+	for (var i = refData.length; i > 0;) 
+		{
+			// tricky reverse cycles again
+			delta += Math.abs (refData[--i] - newData[i]) 
+			+ Math.abs (refData[--i] - newData[i]) 
+			+ Math.abs (refData[--i] - newData[i]) 
+			+ Math.abs (refData[--i] - newData[i]);
+		}
 	//post message back to main script
 	postMessage (delta)
 }

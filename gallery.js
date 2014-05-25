3af5a434fa7bb094f29d6b780986bc2b5c73e2d2
infoburp <gwolfendale@gmail.com>
2014-05-25 03:38:38 +0100

    gallery

---
+++ gallery.js
@@ -0,0 +1,12 @@
+
+	//loop for number of images to get
+	for ()
+		{
+			//get imageid for (next)best % match from database
+			//get gallery(imageid) from imgur
+			//add image to gallery div html
+			galleryDiv.html += ''
+			//get svg(imageid) from database
+			//add svg to gallery div html
+			galleryDiv.html += ''
+		}

9e89fa11846aada9707b52e3a550ce0d51b91725
infoburp <gwolfendale@gmail.com>
2014-05-25 03:57:44 +0100

    gallery

--- gallery.js
+++ gallery.js
@@ -1,12 +1,18 @@
 
+numImagesInput = document.getElementById('num-image');
+numberOfImages = parseInt (numImagesInput.value);
+galleryDiv = document.getElementById('gallery');
+window.onload = function(e)
+{
 	//loop for number of images to get
-	for ()
+	for (var i=0; i<22; i++)
 		{
 			//get imageid for (next)best % match from database
 			//get gallery(imageid) from imgur
 			//add image to gallery div html
-			galleryDiv.html += ''
+			galleryDiv.innerHTML += 'original';
 			//get svg(imageid) from database
 			//add svg to gallery div html
-			galleryDiv.html += ''
+			galleryDiv.innerHTML += 'render';
 		}
+}
